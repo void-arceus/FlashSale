eb4f9cfd2e49c0f6411f049f759bdbd451c320a9
void-arceus <vt79328@gmail.com>
2026-07-04 17:53:10 +0530

    feat: implement flashsale coordinator layout with dynamic react-form-hook product selector

--- frontend/src/features/admin/pages/SaleCoordinator.tsx
+++ frontend/src/features/admin/pages/SaleCoordinator.tsx
@@ -1,40 +1,135 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { getAdminProducts } from "../../products/services/productService";
+import type { Product } from "../../products/services/productService";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { useAuth } from "../../../context/AuthContext";
+
+interface FlashSaleInput {
+    productId: string;
+    originalPrice: number;
+    salePrice: number;
+    saleStartTime: string;
+    saleEndTime: string;
+}
 
 export default function SaleCoordinator() {
     // get the product names and original price
-    useEffect(() => {}, []);
+    const [products, setProducts] = useState<Product[] | null>(null);
+    const { user } = useAuth();
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<FlashSaleInput>({
+        defaultValues: {
+            productId: "",
+            originalPrice: undefined,
+            salePrice: undefined,
+            saleStartTime: "",
+            saleEndTime: "",
+        },
+    });
+
+    const watchProductId = watch("productId");
+    const selectedProduct =
+        products?.find((p) => p._id === watchProductId) || null;
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    const onSubmit: SubmitHandler<FlashSaleInput> = async (data) => {
+        console.log("You form is being submitted", data);
+    };
+
+    const getProducts = async () => {
+        if (user && user.role === "admin") {
+            const res = await getAdminProducts(user.id);
+            if (res.status === true) {
+                setProducts(res.data as Product[]);
+            } else {
+                setProducts(null);
+            }
+        }
+    };
 
     return (
-        <main className="h-screen w-full pt-18 flex flex-col items-center gap-6">
+        <main className="h-screen w-full pt-18 flex flex-col items-center gap-2">
             {/* header */}
-            <section className="h-fit w-full max-w-6xl flex flex-col items-center justify-center gap-2">
-                <h1 className="text-xl text-text-main font-semibold">
+            <section className="h-fit w-full max-w-6xl flex flex-col items-center justify-center gap-2 p-4">
+                <h1 className="text-xl text-text-main font-semibold text-center">
                     Flash Sale Coordinator
                 </h1>
-                <p className="text-sm font-medium text-text-muted">
+                <p className="text-sm font-medium text-text-muted text-center">
                     Manage your timed store promotions and active countdowns
                     here.
                 </p>
             </section>
 
             {/* start new sale and view on going or scheduled sales  */}
-            <section className="w-full max-w-6xl grid grid-cols-1 md:grid-cols-2 p-4 gap-6">
+            <section className="w-full max-w-6xl grid grid-cols-1 md:grid-cols-2 p-4 gap-4">
                 <div className="h-fit w-full border border-border rounded-xl p-3 flex flex-col items-center gap-4 shadow-lg">
-                    <h1>Schedule New Sale</h1>
-                    <div>
-                        <h2>Product Name will appear here</h2>
-                    </div>
-                    <div>
-                        <p>Set Sale start time</p>
-                    </div>
-                    <div>
-                        <p>set sale end time</p>
-                    </div>
-                    <div className="w-full flex items-center justify-end">
-                        <button className="px-4 py-2 bg-btn-primary hover:bg-btn-hover hover:cursor-pointer rounded-lg text-btn-text active:scale-[0.981] transition-scale duration-200 ease-in-out">
-                            Schedule Sale
-                        </button>
-                    </div>
+                    <form
+                        onSubmit={handleSubmit(onSubmit)}
+                        className="w-full flex flex-col items-center gap-6"
+                    >
+                        <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-2">
+                            <div className="w-full flex flex-col items-start gap-2">
+                                <div className="w-full flex items-center justify-start gap-2">
+                                    <label
+                                        htmlFor="selectProduct"
+                                        className="text-sm font-medium text-text-muted"
+                                    >
+                                        Select Product:
+                                    </label>
+                                    <select
+                                        id="selectProduct"
+                                        {...register("productId", {
+                                            required: "Please select a product",
+                                        })}
+                                        className="border border-border p-2 rounded-lg text-sm text-text-main font-medium cursor-pointer"
+                                    >
+                                        <option value="">
+                                            --Choose an item--
+                                        </option>
+                                        {products?.map((p) => (
+                                            <option value={p._id} key={p._id}>
+                                                {p.productName}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                {errors && (
+                                    <p className="text-sm text-error font-medium">
+                                        {errors.productId?.message}
+                                    </p>
+                                )}
+                            </div>
+                            {selectedProduct ? (
+                                <div className="w-full flex items-center">
+                                    <h2 className="text-sm text-text-main font-medium">
+                                        Original Price:&nbsp;
+                                    </h2>
+                                    <p className="text-sm font-semibold text-text-muted">
+                                        ₹{selectedProduct.originalPrice}
+                                    </p>
+                                </div>
+                            ) : null}
+                        </div>
+                        <div className="w-full flex sm:flex-row flex-col items-center justify-end gap-2">
+                            <button
+                                type="button"
+                                className="border border-border px-4 py-2 rounded-lg bg-surface text-sm text-text-primary font-medium hover:cursor-pointer shadow-md hover:shadow-lg active:scale-[0.98] transition-scale duration-200 ease-in-out"
+                            >
+                                Add New Product
+                            </button>
+                            <button className="px-4 py-2 rounded-lg bg-btn-primary text-sm text-btn-text font-medium hover:cursor-pointer hover:bg-btn-hover shadow-md hover:shadow-lg active:scale-[0.98] transition-scale duration-200 ease-in-out">
+                                Schedule Sale
+                            </button>
+                        </div>
+                    </form>
                 </div>
                 <div className="h-fit w-full border border-border rounded-xl p-2 shadow-lg">
                     View ongoing and existing sales with timer
--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -2,38 +2,56 @@ import { useState, useEffect } from "react";
 import { getProducts } from "../features/products/services/productService";
 import type { Product } from "../features/products/services/productService";
 import GeneralProductCard from "./GeneralProductCard";
+import Loading from "../components/ui/Loading";
 
 function Homepage() {
-    const [products, setProducts] = useState<Product[]>([]);
+    const [products, setProducts] = useState<Product[] | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         handleGetProducts();
     }, []);
 
     const handleGetProducts = async () => {
-        const result = await getProducts();
-        if (result.status && result.data) {
-            setProducts(result.data);
+        try {
+            setLoading(true);
+            const result = await getProducts();
+            if (result.status && result.data) {
+                setProducts(result.data);
+            }
+        } catch (error: any) {
+            1;
+            setLoading(false);
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
         <main className="pt-16 w-full flex items-center justify-center">
-            <section className="relative w-full max-w-6xl flex flex-col">
-                {/* testing displaying product data*/}
-                <h1 className="px-4 text-2xl font-semibold text-text-main mb-3">
-                    All Products
-                </h1>
-                <div className="w-full px-4 pb-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                    {products.length > 0 ? (
-                        products.map((p) => (
-                            <GeneralProductCard product={p} key={p._id} />
-                        ))
-                    ) : (
-                        <div>No Products Found</div>
-                    )}
+            {loading ? (
+                <div className="h-screen w-full flex items-center justify-center">
+                    <Loading />
                 </div>
-            </section>
+            ) : (
+                <section className="relative w-full max-w-6xl flex flex-col">
+                    {/* testing displaying product data*/}
+                    <h1 className="px-4 text-2xl font-semibold text-text-main mb-3">
+                        All Products
+                    </h1>
+                    {products ? (
+                        <div className="w-full px-4 pb-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                            {products.map((p) => (
+                                <GeneralProductCard product={p} key={p._id} />
+                            ))}
+                        </div>
+                    ) : loading ? null : (
+                        <div className="h-screen w-full flex items-center justify-center text-3xl font-semibold text-error">
+                            No Products Found
+                        </div>
+                    )}
+                </section>
+            )}
         </main>
     );
 }
